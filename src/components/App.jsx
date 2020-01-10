3074de9196b6e222146ddd5429d1f6ac6f5c275b
Lee Raulin <leeraulin@gmail.com>
2020-01-10 12:55:06 -0500

    Revert "Revert "..."" back again
    This reverts commit acb06190ec913931de2c42943abfe2f56ef83edf.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,9 @@ export const App = () => {
   };
 
   // Once after mounting
-  useEffect(() => BookStore.addChangeListener(onBookChange), []);
+  useEffect(() => {
+    BookStore.addChangeListener(onBookChange);
+  }, []);
 
   // Before component is removed
   useEffect(() => () => BookStore.removeChangeListener(onBookChange), []);
